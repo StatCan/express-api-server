659df5577bf3fe460ce9e5611069a7c87e233790
Laurent Goderre <laurent.goderre@gmail.com>
2019-01-25 09:58:32 -0500

    Added an easier and safer way to register api endpoints

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const {APIError, RequestError, DataError} = require('./errors');
 
-const setupEndpoint = (fn, router, options) => {
-	if (typeof fn === 'function') {
-		fn(router);
-	}
-};
-
-module.exports = function(endpoints, options = {port: 8000}) {
+module.exports = function(endpointsObject = {}, options = {port: 8000}) {
 	const app = express();
 	const router = new express.Router();
+	const endpoints = [];
 	const start = () => {
+		router.use((req, res, next) => {
+			if (res.locals.json) {
+				return res.jsonp(res.locals.json);
+			}
+			next();
+		});
+
 		app.get('/:endpoint*', (req, res, next) => {
 			next(new RequestError(req, 404, `Endpoint '${req.params.endpoint}' not found`));
 		});
@@ -20,7 +22,7 @@ module.exports = function(endpoints, options = {port: 8000}) {
 				if (err instanceof DataError)
 					process.stderr.write(`Error: ${err.message} (URL: ${req.url})\n`);
 
-				return res.status(err.status).json({
+				return res.status(err.status).jsonp({
 					errors: [
 						{
 							title: err.message
@@ -29,14 +31,14 @@ module.exports = function(endpoints, options = {port: 8000}) {
 				});
 			}
 			process.stderr.write(`${req.url}\nError: ${err.stack}\n`);
-			res.status(500).json({
+			res.status(500).jsonp({
 				errors: [
 					{
 						title: 'Unknown Internal Server Error'
 					}
 				]
 			});
-			next(err);
+			next();
 		});
 
 		return app.listen(options.port);
@@ -44,12 +46,28 @@ module.exports = function(endpoints, options = {port: 8000}) {
 
 	router.use((req, res, next) => next());
 
-	if (Array.isArray(endpoints)) {
-		for (const endpoint of endpoints) {
-			setupEndpoint(endpoint, router, options);
+	router.get('/', (req, res, next) => {
+		res.locals.json = {
+			data: endpoints.map((endpoint) => {
+				return {
+					type: 'endpoint',
+					id: endpoint,
+					links: {
+						self: `/${endpoint}`
+					}
+				};
+			})
+		};
+		next();
+	});
+
+	for (const [endpointName, route] of Object.entries(endpointsObject)) {
+		endpoints.push(endpointName);
+		for (const [routePath, routeFn] of Object.entries(route)) {
+			if (routeFn && typeof routeFn === 'function') {
+				routeFn(router.route(`/${endpointName}/${routePath.replace(/^\/*/, '')}`.replace(/\/*$/, '')));
+			}
 		}
-	} else {
-		setupEndpoint(endpoints, router, options);
 	}
 
 	app.use('/', router);
